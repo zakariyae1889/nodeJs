3d5966be88e0fe42bfa19314a25df6a9af9df866
ZakariyaeABBIH <75364734+zakariyae1889@users.noreply.github.com>
2025-11-05 15:16:45 +0100

    Add files via upload

---
+++ config/allowedOrigins.js
@@ -0,0 +1,3 @@
+const newLocal = ["http://localhost:3000"];
+const allowedOrigins=newLocal;
+export default allowedOrigins
---
+++ config/corsOption.js
@@ -0,0 +1,14 @@
+import allowedOrigins from "./allowedOrigins.js";
+
+export const corsOption={
+    origin:(origin,callback)=>{
+        if(allowedOrigins.indexOf(origin)!==-1 || !origin){
+            callback(null,true);
+
+        }else{
+            callback(new Error("Not allowed by cors"));
+        }
+    },
+    credentials:true,
+    optionsSuccessStatus:200
+}
